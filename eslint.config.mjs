7b000452900d7c642067ed0b31cdbf48fab25f57
Alex Belik <alex.belik26@gmail.com>
2024-09-19 10:51:09 +0200

    formatted

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,31 +3,18 @@ import pluginJs from '@eslint/js';
 
 export default [
   {
-    ignores:
-      [
-        'node_modules',
-      ],
+    ignores: ['node_modules'],
   },
   {
-    files:
-      [
-        '**/*.js',
-      ],
-    languageOptions:
-      {
-        ecmaVersion:
-          'latest',
-        sourceType:
-          'commonjs',
-      },
-    rules:
-      {
-        eqeqeq:
-          'error',
-        'no-unused-vars':
-          'error',
-        'prefer-const':
-          'error',
-      },
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+    },
+    rules: {
+      eqeqeq: 'error',
+      'no-unused-vars': 'error',
+      'prefer-const': 'error',
+    },
   },
 ];
