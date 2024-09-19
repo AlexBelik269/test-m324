b9ddce75d4c021cb684a5aba7156b27efed1866d
Alex Belik <alex.belik26@gmail.com>
2024-09-19 10:46:15 +0200

    linter fixed

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const chalk = require('chalk');
 console.log(chalk.blue('It works'));
 
-
-let myUnusedVariable = 42; // Unused variable and should be 'const'
 const UsedVariable = 42;
 
-if (UsedVariable == 42) { // Should use '===' instead of '=='
+if (UsedVariable === 42) { 
   console.log('This will trigger ESLint errors');
 }
